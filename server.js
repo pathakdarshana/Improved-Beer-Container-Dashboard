f89585526ea3d9e0af29f9118ed3726753023f18
Darshana Pathak <darshana.pathak@tabcorp.com.au>
2018-05-01 23:00:18 +1000

    Added routes for beer

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser');
 
-const app = express();
-
+const app = require('./src/app');
 
 const port = 8000;
 
 app.listen( port, () => {
-	console.log('We are live on port'+ port);
+	console.log('We are live on port : '+ port);
 });
+
+ 
